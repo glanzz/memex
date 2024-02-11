1f67268de646c58bbb8f4dfed3e094ae61d4249f
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-02-11 04:17:59 -0500

    Document logger functions

--- app/logger.py
+++ app/logger.py
@@ -3,13 +3,16 @@ from app.config import DEBUG
 class Logger:
   @classmethod
   def message(cls, message):
+    '''Message to be visible even when debug is off'''
     print(message)
   
   @classmethod
   def error(cls, error):
+    '''Error which requires attention shows even if debug is off'''
     print(error)
   
   @classmethod
   def debug(cls, debug_message):
+    '''Use only for development purpose: Shown only on debug mode'''
     if DEBUG:
       print(debug_message)
