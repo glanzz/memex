84eb8b63703bcba5e31432da0c9a769dfb518aa6
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-02-14 11:11:41 -0500

    Add headers required for encoding / compression

--- app/constants/headers.py
+++ app/constants/headers.py
@@ -2,3 +2,6 @@ CONTENT_TYPE = "content-type"
 CONTENT_LENGTH = "content-length"
 LOCATION = "location"
 CACHE_CONTROL = "cache-control"
+CONTENT_ENCODING = 'content-encoding'
+ACCEPT_ENCODING = 'accept-encoding'
+TRANSFER_ENCODING = 'transfer-encoding'
