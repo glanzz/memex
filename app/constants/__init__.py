2e647c79f2bad5a241bbee363a55617800890d8a
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-02-22 01:09:51 -0500

    Draw to screen

--- app/constants/__init__.py
+++ app/constants/__init__.py
@@ -1,3 +1,4 @@
 from app.constants.common import *
 from app.constants.headers import *
 from app.constants.cache import *
+from app.constants.window import *
--- app/memex.py
+++ app/memex.py
@@ -1,16 +1,34 @@
+import tkinter
 from app.logger import Logger
 from app.schemes import HTTPScheme
-from app.constants import ENTITY_SYMBOL_MAPPING, DEFAULT_URL
+from app.constants import ENTITY_SYMBOL_MAPPING, DEFAULT_URL, WIDTH, HEIGHT, HSTEP, VSTEP, SCROLL_STEP
 from app.URL import URL
 from app.Cache import Cache
 
 
 class Memex:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+
     def __enter__(self):
         Cache.safe_init_folder()
         return Memex()
+    
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+    
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.draw()
 
     def show(self, body, encoding, view_mode=False):
+        text = ""
         show_data = body.decode(encoding if encoding else "utf-8")
         in_tag = False
 
@@ -21,7 +39,7 @@ class Memex:
                 continue
 
             if view_mode:
-                print(show_data[i], end="")
+                text += show_data[i] #print(show_data[i], end="")
                 continue
 
             if show_data[i] == "<":
@@ -31,28 +49,48 @@ class Memex:
             elif show_data[i] == "&":
                 remaining_string = show_data[i + 1 :]
                 if not remaining_string:
-                    print(show_data[i], end="")
+                    text += show_data[i]
                     break
                 token, remaining_string = remaining_string.split(";", 1)
                 token_value = ENTITY_SYMBOL_MAPPING.get(token)
                 if token_value:
-                    print(token_value, end="")
+                    text += token_value
                     if remaining_string == "":
                         break
                     skip_till = show_data.find(remaining_string)
                 else:
-                    print(show_data[i], end="")
+                    text += show_data[i]
             elif not in_tag:
-                print(show_data[i], end="")
+                text += show_data[i]
+        return text
 
     def load(self, url=DEFAULT_URL):
         url = URL(url=url if url else DEFAULT_URL)
         url.scheme_request.request()
-        self.show(
+        content = self.show(
             url.scheme_request.body,
             encoding=url.scheme_request.body_encoding,
             view_mode=url.get_view_mode(),
         )
+        self.layout(content=content)
+        self.draw()
+    
+    def layout(self, content):
+        self.layout_list = []
+        cursor_x, cursor_y = HSTEP, VSTEP
+        for c in content:
+            self.layout_list.append((cursor_x, cursor_y, c))
+            cursor_x += HSTEP
+            if cursor_x > WIDTH-HSTEP:
+                cursor_x = HSTEP
+                cursor_y += HSTEP
+    
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.layout_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def __exit__(self, *args):
         Logger.message("Closing all sockets...")
