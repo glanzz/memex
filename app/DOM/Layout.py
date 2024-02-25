1b132847eac6dd5dd094c1b784d7a31a3f336d2c
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-02-24 19:42:49 -0500

    Support font size alterations

--- app/DOM/Layout.py
+++ app/DOM/Layout.py
@@ -1,19 +1,22 @@
-import tkinter.font as TKFont
-from app.constants import HSTEP, VSTEP, FontStyle, FontWeight, DEFAULT_LINE_HEIGHT
+from tkinter import font as TKFont
+from tkinter import Label
+from app.constants import HSTEP, VSTEP, FontStyle, FontWeight, DEFAULT_LINE_HEIGHT, FontSize
 from app.DOM import Text
 
 class FontCache:
   def __init__(self):
     self.cache = {}
   
-  def get_font(self, weight, style):
-    if not self.cache.get((weight, style)):
-      self.cache[(weight, style)] = TKFont.Font(
-        size=16,
+  def get_font(self, weight, style, size):
+    if not self.cache.get((weight, style, size)):
+      font = TKFont.Font(
+        family='Helvetica',
+        size=size,
         weight=weight,
         slant=style,
       )
-    return self.cache[(weight, style)]
+      self.cache[(weight, style, size)] = (font, Label(font=font))
+    return self.cache[(weight, style, size)][0]
 
     
 
@@ -29,31 +32,45 @@ class Layout:
       self.content_height = self.height
       self.weight = FontWeight.NORMAL.value
       self.style = FontStyle.ROMAN.value
-      self.size = 16
+      self.size = FontSize.DEFAULT.value
+
+      self.line = []
+
       self.generate_layout(tokens)
 
   def generate_layout(self, tokens):
     for token in tokens:
         self.token(token=token)
+    self.flush()
     self.content_height = self.cursor_y
   
 
   def add_word(self, word):
     # Handle new line character
-    if not word or (word == "\n"):
-        self.cursor_y += font.metrics("linespace") * DEFAULT_LINE_HEIGHT
-        self.cursor_x = HSTEP
+    font = self.font_cache.get_font(self.weight, self.style, self.size)
+
+    if word == "" or (word == "\n"):
+        self.flush()
         return
 
-    font = self.font_cache.get_font(self.weight, self.style)
-    
+    word_width = font.measure(word)            
+    if (self.cursor_x + word_width) > (self.width - HSTEP):
+        self.flush()
+    self.line.append((self.cursor_x, word, font))
+    self.cursor_x += (word_width + font.measure(" "))
+  
+  def flush(self):
+    if not self.line: return
+    max_ascent = max([font.metrics("ascent") for x, word, font in self.line])
+    baseline = self.cursor_y + (DEFAULT_LINE_HEIGHT * max_ascent)
+    for x, word, font in self.line:
+      y = baseline - font.metrics("ascent")
+      self.display_list.append((x,y,word,font))
+    max_descent = max([font.metrics("descent") for x, word, font in self.line])
+    self.cursor_y = baseline + (DEFAULT_LINE_HEIGHT * max_descent)
+    self.cursor_x = HSTEP
+    self.line = []
 
-    word_width = font.measure(word)             
-    if self.cursor_x + word_width > self.width - HSTEP:
-        self.cursor_x = HSTEP
-        self.cursor_y += font.metrics("linespace") * DEFAULT_LINE_HEIGHT
-    self.display_list.append((self.cursor_x, self.cursor_y, word, font))
-    self.cursor_x += (word_width + font.measure(" ")) # Add word and a space
 
   def token(self, token):
     if isinstance(token, Text):
@@ -67,5 +84,18 @@ class Layout:
         self.weight = FontWeight.BOLD.value
     elif token.tag == "/b":
         self.weight = FontWeight.NORMAL.value
+    elif token.tag == "big":
+        self.size = FontSize.BIG.value
+    elif token.tag == "/big":
+        self.size = FontSize.DEFAULT.value
+    elif token.tag == "small":
+        self.size = FontSize.SMALL.value
+    elif token.tag == "/small":
+        self.size = FontSize.DEFAULT.value
+    elif token.tag == "br/" or token.tag == "br":
+      self.flush()
+    elif token.tag == "/p":
+      self.flush()
+      self.cursor_y += VSTEP
 
 
--- app/constants/font.py
+++ app/constants/font.py
@@ -8,5 +8,10 @@ class FontStyle(Enum):
   ITALIC = "italic"
   ROMAN = "roman"
 
+class FontSize(Enum):
+  DEFAULT = 14
+  BIG = 16
+  SMALL = 12
+
 
 DEFAULT_LINE_HEIGHT = 1.25
